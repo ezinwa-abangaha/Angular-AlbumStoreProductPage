1eb43a55250aa53228c30ee44fa87241fe0307c1
Ezinwa Abangaha <abangahaezinwa@gmail.com>
2019-11-02 14:21:19 -0700

    product changes

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -8,12 +9,12 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(private _productService: ProductService) { }
- albumInfo;
+  albumInfo;
 
-  ngOnInit() {
-    this._productService.getAlbum(1). subscribe
-    ( response =>this.albumInfo= response);
+  constructor(private _productService: ProductService) { }
+ 
+ ngOnInit() {
+    this._productService.getAlbum(1).subscribe( response =>this.albumInfo = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Http Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
-@Injectable()
+@Injectable() 
 export class ProductService {
 
 private _albumUrl = '../assets/album.json';
@@ -11,8 +11,7 @@ private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this.albumUrl).map((response)
-     => response.json ());
+    return this._http.get(this._albumUrl).map((response) => response.json ());
   }
 
 }
