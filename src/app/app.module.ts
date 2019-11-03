104ce86353e1a9d2e0e05613e5dcaab833f3d704
Ezinwa Abangaha <abangahaezinwa@gmail.com>
2019-11-03 10:45:59 -0800

    app.modules changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,10 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'products, component: ProductListComponent'},
+  { path: 'products, component: ProductListComponent' },
   { path: 'product/:id', component: ProductPageComponent },
-  { path: '', redirectTo: 'products', pathMatch:'full'},
-],
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+];
 
 @NgModule({
 declarations: [
@@ -29,7 +29,7 @@ declarations: [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
