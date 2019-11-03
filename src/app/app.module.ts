5c2ef074477de9f2a01f45f83609c95780fed006
Ezinwa Abangaha <abangahaezinwa@gmail.com>
2019-11-03 10:58:31 -0800

    app.modules channels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'products, component: ProductListComponent' },
+  { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductPageComponent },
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ];
 
 @NgModule({
